90c9756b1210713e8870fc4881d08c0a93a3d2c5
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-01-23 20:11:35 +0000

    Add fetchGitHubinformation function to githubinformation file in javascript subfolder of assets folder

---
+++ assets/js/github-information.js
@@ -0,0 +1,16 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
